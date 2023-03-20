e0b2328dd8fe05264d764ee329f0aead76086e1d
IacobIonut01 <paulionut2003@gmail.com>
2023-03-20 14:58:23 +0200

    Glide: Improve cache
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/src/main/java/com/dot/gallery/injection/GalleryGlideModule.kt
+++ app/src/main/java/com/dot/gallery/injection/GalleryGlideModule.kt
@@ -3,6 +3,7 @@ package com.dot.gallery.injection
 import android.content.Context
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
 import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.load.engine.cache.MemorySizeCalculator
@@ -12,10 +13,10 @@ import com.bumptech.glide.module.AppGlideModule
 class GalleryGlideModule : AppGlideModule() {
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         val calculator = MemorySizeCalculator.Builder(context)
-            .setMemoryCacheScreens(4f)
+            .setMemoryCacheScreens(2f)
             .build()
         builder.setIsActiveResourceRetentionAllowed(true)
-        builder.setDiskCache(InternalCacheDiskCacheFactory(context));
+        builder.setDiskCache(DiskLruCacheFactory("${context.cacheDir}/image_cache", 100 * 1024 * 1024))
         builder.setMemoryCache(LruResourceCache(calculator.memoryCacheSize.toLong()))
     }
 }
