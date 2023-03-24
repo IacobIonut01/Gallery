6c560ea3538e2b2e27438fc1fbb5e00a6794d7fc
IacobIonut01 <paulionut2003@gmail.com>
2023-03-24 21:14:16 +0200

    Gradle: Move to Kotlin script
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,61 +1,61 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
-    id 'kotlin-kapt'
-    id 'kotlin-parcelize'
-    id 'dagger.hilt.android.plugin'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
-    namespace 'com.dot.gallery'
-    compileSdk 33
+    namespace = "com.dot.gallery"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId "com.dot.gallery"
-        minSdk 33
-        targetSdk 33
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.dot.gallery"
+        minSdk = 33
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
 
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
-            useSupportLibrary true
+            useSupportLibrary = true
         }
     }
 
     lintOptions {
-        baseline file("lint-baseline.xml")
+        baseline(file("lint-baseline.xml"))
     }
 
     buildTypes {
-        release {
-            minifyEnabled true
-            shrinkResources true
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-            signingConfig signingConfigs.debug
+        named("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            setProguardFiles(listOf(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"))
+            signingConfig = signingConfigs.getByName("debug")
         }
-        staging {
-            initWith release
-            minifyEnabled false
-            shrinkResources false
+        register("staging") {
+            initWith(getByName("release"))
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
     buildFeatures {
-        compose true
+        compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion '1.4.2'
+        kotlinCompilerExtensionVersion = "1.4.2"
     }
     packagingOptions {
         resources {
-            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
@@ -65,12 +65,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
-    implementation 'androidx.navigation:navigation-runtime-ktx:2.5.3'
+    implementation("androidx.navigation:navigation-runtime-ktx:2.5.3")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
 
     // Compose
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation platform("androidx.compose:compose-bom:2023.01.00")
+    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -85,7 +85,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     // Dagger - Hilt
-    implementation "androidx.hilt:hilt-navigation-compose:1.1.0-alpha01"
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
     implementation("com.google.dagger:hilt-android:2.45")
     kapt("com.google.dagger:hilt-android-compiler:2.45")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
@@ -103,14 +103,14 @@ dependencies {
     kapt("com.github.bumptech.glide:compiler:4.15.1")
 
     // Exo Player
-    implementation 'androidx.media3:media3-exoplayer:1.0.0-rc02'
-    implementation 'androidx.media3:media3-ui:1.0.0-rc02'
+    implementation("androidx.media3:media3-exoplayer:1.0.0-rc02")
+    implementation("androidx.media3:media3-ui:1.0.0-rc02")
 
     // Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation platform("androidx.compose:compose-bom:2023.01.00")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.01.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url 'https://jitpack.io' }
+        maven("https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
@@ -11,8 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven("https://jitpack.io")
     }
 }
 rootProject.name = "Gallery"
-include ':app'
+include(":app")
