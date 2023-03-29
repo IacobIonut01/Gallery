1ec65122722c17d447461dad0fdb40b950ee6a46
IacobIonut01 <paulionut2003@gmail.com>
2023-03-29 23:16:40 +0300

    Gradle: Update compose bom to 2023.03.00
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
 
     // Compose
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -109,7 +109,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.01.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
