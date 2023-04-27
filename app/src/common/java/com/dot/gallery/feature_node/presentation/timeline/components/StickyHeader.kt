07e490b36897c2ab48905c21fe19cb1548fe39ec
IacobIonut01 <paulionut2003@gmail.com>
2023-04-27 22:11:42 +0300

    StickyHeader: Add long click on date to select the group
    Improves the UX by allowing user to long press the date header and select the whole group even if Selection is not active (0 items selected)
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/src/common/java/com/dot/gallery/feature_node/presentation/timeline/components/StickyHeader.kt
+++ app/src/common/java/com/dot/gallery/feature_node/presentation/timeline/components/StickyHeader.kt
@@ -5,6 +5,10 @@
 
 package com.dot.gallery.feature_node.presentation.timeline.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,11 +19,15 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.dot.gallery.core.Constants.Animation.enterAnimation
+import com.dot.gallery.core.Constants.Animation.exitAnimation
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun StickyHeader(
     date: String,
@@ -49,14 +57,33 @@ fun StickyHeader(
         Text(
             text = date,
             style = if (showAsBig) bigTextStyle else smallTextStyle,
-            color = MaterialTheme.colorScheme.onSurface
-        )
-        if (!showAsBig && isCheckVisible.value) {
-            RadioButton(
-                modifier = Modifier.height(22.dp),
-                selected = isChecked.value,
-                onClick = onChecked
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.then(
+                if (!showAsBig) Modifier.combinedClickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onLongClick = {
+                        isCheckVisible.value = !isCheckVisible.value
+                        onChecked()
+                    },
+                    onClick = {
+                        if (isCheckVisible.value) onChecked()
+                    }
+                ) else Modifier
             )
+        )
+        if (!showAsBig) {
+            AnimatedVisibility(
+                visible = isCheckVisible.value,
+                enter = enterAnimation,
+                exit = exitAnimation
+            ) {
+                RadioButton(
+                    modifier = Modifier.height(22.dp),
+                    selected = isChecked.value,
+                    onClick = onChecked
+                )
+            }
         }
     }
 }
