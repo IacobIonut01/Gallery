014c361dacc36b63b505e5f2cd674d1d7fae0151
IacobIonut01 <paulionut2003@gmail.com>
2023-06-25 16:57:06 +0300

    Glide: Load settings inside a coroutine scope
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/src/main/kotlin/com/dot/gallery/injection/GalleryGlideModule.kt
+++ app/src/main/kotlin/com/dot/gallery/injection/GalleryGlideModule.kt
@@ -18,38 +18,43 @@ import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
 import com.dot.gallery.core.Settings.Glide.getCachedScreenCount
 import com.dot.gallery.core.Settings.Glide.getDiskCacheSize
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 
 @GlideModule
 class GalleryGlideModule : AppGlideModule() {
     override fun applyOptions(context: Context, builder: GlideBuilder) {
-        val diskCacheSize = runBlocking { getDiskCacheSize(context).first() }
-        val screenCount = runBlocking {  getCachedScreenCount(context).first() }
+        CoroutineScope(Dispatchers.Main).launch {
+            val diskCacheSize = getDiskCacheSize(context).first()
+            val screenCount = getCachedScreenCount(context).first()
 
-        builder.setDefaultRequestOptions(
-            RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
-        )
-        val memoryCalculator = MemorySizeCalculator.Builder(context)
-            .setMemoryCacheScreens(screenCount)
-            .build()
-        builder.setMemoryCache(LruResourceCache(memoryCalculator.memoryCacheSize.toLong()))
-        builder.setDiskCache(
-            DiskLruCacheFactory(
-                "${context.cacheDir}/image_cache",
-                diskCacheSize * 1024 * 1024
+            builder.setDefaultRequestOptions(
+                RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             )
-        )
-        val bitmapCalculator = MemorySizeCalculator.Builder(context)
-            .setBitmapPoolScreens(screenCount)
-            .build()
-        builder.setBitmapPool(LruBitmapPool(bitmapCalculator.bitmapPoolSize.toLong()))
-        builder.setImageDecoderEnabledForBitmaps(true)
-        builder.setSourceExecutor(
-            GlideExecutor
-                .newSourceBuilder()
-                .setThreadCount(GlideExecutor.calculateBestThreadCount())
+            val memoryCalculator = MemorySizeCalculator.Builder(context)
+                .setMemoryCacheScreens(screenCount)
                 .build()
-        )
+            builder.setMemoryCache(LruResourceCache(memoryCalculator.memoryCacheSize.toLong()))
+            builder.setDiskCache(
+                DiskLruCacheFactory(
+                    "${context.cacheDir}/image_cache",
+                    diskCacheSize * 1024 * 1024
+                )
+            )
+            val bitmapCalculator = MemorySizeCalculator.Builder(context)
+                .setBitmapPoolScreens(screenCount)
+                .build()
+            builder.setBitmapPool(LruBitmapPool(bitmapCalculator.bitmapPoolSize.toLong()))
+            builder.setImageDecoderEnabledForBitmaps(true)
+            builder.setIsActiveResourceRetentionAllowed(true)
+            builder.setSourceExecutor(
+                GlideExecutor
+                    .newSourceBuilder()
+                    .setThreadCount(GlideExecutor.calculateBestThreadCount())
+                    .build()
+            )
+        }
     }
 }
