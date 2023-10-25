811e0872e009de128cce4007694f5cd4686a7903
IacobIonut01 <paulionut2003@gmail.com>
2023-10-26 00:12:19 +0300

    Gallery: Replace dropdown menu on timeline with new bottom sheet
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

---
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/common/components/OptionSheet.kt
@@ -0,0 +1,194 @@
+package com.dot.gallery.feature_node.presentation.common.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.dot.gallery.core.presentation.components.DragHandle
+import com.dot.gallery.feature_node.presentation.common.components.OptionPosition.ALONE
+import com.dot.gallery.feature_node.presentation.common.components.OptionPosition.BOTTOM
+import com.dot.gallery.feature_node.presentation.common.components.OptionPosition.MIDDLE
+import com.dot.gallery.feature_node.presentation.common.components.OptionPosition.TOP
+import com.dot.gallery.feature_node.presentation.util.AppBottomSheetState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OptionSheet(
+    state: AppBottomSheetState,
+    onDismiss: (() -> Unit)? = null,
+    vararg optionList: List<OptionItem>
+) {
+    val scope = rememberCoroutineScope()
+    if (state.isVisible) {
+        ModalBottomSheet(
+            sheetState = state.sheetState,
+            onDismissRequest = {
+                scope.launch {
+                    onDismiss?.invoke()
+                    state.hide()
+                }
+            },
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+            tonalElevation = 0.dp,
+            dragHandle = { DragHandle() },
+            windowInsets = WindowInsets(0, 0, 0, 0)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+                    .padding(horizontal = 32.dp, vertical = 16.dp)
+                    .navigationBarsPadding()
+            ) {
+                optionList.forEach { list ->
+                    OptionLayout(
+                        modifier = Modifier.fillMaxWidth(),
+                        optionList = list
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun OptionLayout(
+    modifier: Modifier = Modifier,
+    optionList: List<OptionItem>
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(1.dp)
+    ) {
+        optionList.forEachIndexed { index, item ->
+            val position: OptionPosition = if (index == 0) {
+                if (optionList.size == 1) ALONE
+                else TOP
+            } else if (index == optionList.size - 1) BOTTOM
+            else MIDDLE
+            OptionButton(
+                modifier = Modifier.fillMaxWidth(),
+                textContainer = {
+                    Text(text = item.text)
+                },
+                enabled = item.enabled,
+                containerColor = item.containerColor
+                    ?: MaterialTheme.colorScheme.surfaceContainerHigh,
+                contentColor = item.contentColor
+                    ?: MaterialTheme.colorScheme.onSurface,
+                position = position,
+                onClick = item.onClick
+            )
+        }
+    }
+}
+
+@Composable
+fun OptionButton(
+    modifier: Modifier = Modifier,
+    containerColor: Color = MaterialTheme.colorScheme.surfaceContainerHigh,
+    contentColor: Color = MaterialTheme.colorScheme.onSurface,
+    textContainer: @Composable () -> Unit,
+    enabled: Boolean = true,
+    position: OptionPosition = ALONE,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 56.dp)
+            .background(
+                color = containerColor,
+                shape = position.shape()
+            )
+            .clip(position.shape())
+            .clickable(
+                enabled = enabled,
+                onClick = onClick
+            )
+            .alpha(if (enabled) 1f else 0.4f)
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        ProvideTextStyle(
+            value = MaterialTheme.typography.bodyMedium.copy(color = contentColor)
+        ) {
+            textContainer()
+        }
+    }
+}
+
+data class OptionItem(
+    val text: String,
+    val onClick: () -> Unit,
+    val enabled: Boolean = true,
+    val containerColor: Color? = null,
+    val contentColor: Color? = null,
+)
+
+object OptionShape {
+
+    val Top = RoundedCornerShape(
+        topEnd = 12.dp,
+        topStart = 12.dp,
+        bottomEnd = 1.dp,
+        bottomStart = 1.dp
+    )
+
+    val Middle = RoundedCornerShape(
+        topEnd = 1.dp,
+        topStart = 1.dp,
+        bottomEnd = 1.dp,
+        bottomStart = 1.dp
+    )
+
+    val Bottom = RoundedCornerShape(
+        topEnd = 1.dp,
+        topStart = 1.dp,
+        bottomEnd = 12.dp,
+        bottomStart = 12.dp
+    )
+
+    val Alone = RoundedCornerShape(
+        topEnd = 12.dp,
+        topStart = 12.dp,
+        bottomEnd = 12.dp,
+        bottomStart = 12.dp
+    )
+}
+
+enum class OptionPosition {
+    TOP, MIDDLE, BOTTOM, ALONE
+}
+
+fun OptionPosition.shape(): RoundedCornerShape = when (this) {
+    TOP -> OptionShape.Top
+    MIDDLE -> OptionShape.Middle
+    BOTTOM -> OptionShape.Bottom
+    ALONE -> OptionShape.Alone
+}
--- app/src/main/kotlin/com/dot/gallery/feature_node/presentation/timeline/components/TimelineNavActions.kt
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/timeline/components/TimelineNavActions.kt
@@ -10,24 +10,28 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MoreVert
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dot.gallery.R
 import com.dot.gallery.core.MediaState
 import com.dot.gallery.feature_node.domain.model.Media
 import com.dot.gallery.feature_node.domain.use_case.MediaHandleUseCase
+import com.dot.gallery.feature_node.presentation.common.components.OptionItem
+import com.dot.gallery.feature_node.presentation.common.components.OptionSheet
 import com.dot.gallery.feature_node.presentation.util.Screen
+import com.dot.gallery.feature_node.presentation.util.rememberAppBottomSheetState
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
@@ -56,76 +60,101 @@ fun TimelineNavActions(
             }
         }
     )
+    val context = LocalContext.current
+    val appBottomSheetState = rememberAppBottomSheetState()
+    LaunchedEffect(appBottomSheetState.isVisible, expandedDropDown.value) {
+        scope.launch {
+            if (expandedDropDown.value) appBottomSheetState.show()
+            else appBottomSheetState.hide()
+        }
+    }
+    val optionList = remember(selectionState.value) {
+        mutableListOf(
+            OptionItem(
+                text = if (selectionState.value)
+                    context.getString(R.string.unselect_all)
+                else
+                    context.getString(R.string.select_all),
+                onClick = {
+                    selectionState.value = !selectionState.value
+                    if (selectionState.value)
+                        selectedMedia.addAll(state.media)
+                    else
+                        selectedMedia.clear()
+                    expandedDropDown.value = false
+                }
+            )
+        ).apply {
+            if (albumId != -1L) {
+                add(
+                    OptionItem(
+                        text = context.getString(R.string.move_album_to_trash),
+                        enabled = !selectionState.value,
+                        onClick = {
+                            scope.launch {
+                                handler.trashMedia(
+                                    result = result,
+                                    mediaList = state.media,
+                                    trash = true
+                                )
+                                navigateUp()
+                            }
+                            expandedDropDown.value = false
+                        }
+                    )
+                )
+            }
+            add(
+                OptionItem(
+                    text = context.getString(R.string.favorites),
+                    enabled = !selectionState.value,
+                    onClick = {
+                        navigate(Screen.FavoriteScreen.route)
+                        expandedDropDown.value = false
+                    }
+                )
+            )
+            add(
+                OptionItem(
+                    text = context.getString(R.string.trash),
+                    enabled = !selectionState.value,
+                    onClick = {
+                        navigate(Screen.TrashedScreen.route)
+                        expandedDropDown.value = false
+                    }
+                )
+            )
+        }
+    }
+    val tertiaryContainer = MaterialTheme.colorScheme.tertiaryContainer
+    val onTertiaryContainer = MaterialTheme.colorScheme.onTertiaryContainer
+    val settingsOption = remember(selectionState.value) {
+        mutableStateListOf(
+            OptionItem(
+                text = context.getString(R.string.settings_title),
+                enabled = !selectionState.value,
+                containerColor = tertiaryContainer,
+                contentColor = onTertiaryContainer,
+                onClick = {
+                    navigate(Screen.SettingsScreen.route)
+                    expandedDropDown.value = false
+                }
+            )
+        )
+    }
     IconButton(onClick = { expandedDropDown.value = !expandedDropDown.value }) {
         Icon(
             imageVector = Icons.Outlined.MoreVert,
             contentDescription = stringResource(R.string.drop_down_cd)
         )
     }
-    DropdownMenu(
-        modifier = Modifier,
-        expanded = expandedDropDown.value,
-        onDismissRequest = { expandedDropDown.value = false }
-    ) {
-        DropdownMenuItem(
-            text = {
-                Text(
-                    text = if (selectionState.value)
-                        stringResource(R.string.unselect_all)
-                    else
-                        stringResource(R.string.select_all)
-                )
-            },
-            onClick = {
-                selectionState.value = !selectionState.value
-                if (selectionState.value)
-                    selectedMedia.addAll(state.media)
-                else
-                    selectedMedia.clear()
-                expandedDropDown.value = false
-            },
-        )
-        if (albumId != -1L) {
-            DropdownMenuItem(
-                text = { Text(text = stringResource(R.string.move_album_to_trash)) },
-                enabled = !selectionState.value,
-                onClick = {
-                    scope.launch {
-                        handler.trashMedia(
-                            result = result,
-                            mediaList = state.media,
-                            trash = true
-                        )
-                        navigateUp()
-                    }
-                    expandedDropDown.value = false
-                },
-            )
-        }
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.favorites)) },
-            enabled = !selectionState.value,
-            onClick = {
-                navigate(Screen.FavoriteScreen.route)
-                expandedDropDown.value = false
-            },
-        )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.trash)) },
-            enabled = !selectionState.value,
-            onClick = {
-                navigate(Screen.TrashedScreen.route)
-                expandedDropDown.value = false
-            },
-        )
-        DropdownMenuItem(
-            text = { Text(text = stringResource(R.string.settings_title)) },
-            enabled = !selectionState.value,
-            onClick = {
-                navigate(Screen.SettingsScreen.route)
-                expandedDropDown.value = false
-            }
-        )
-    }
+
+    OptionSheet(
+        state = appBottomSheetState,
+        onDismiss = {
+            expandedDropDown.value = false
+        },
+        optionList, settingsOption
+    )
 }
 
