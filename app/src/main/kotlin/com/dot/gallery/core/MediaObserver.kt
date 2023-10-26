21979496616fa5fda8e80d2ae1903c6a3af58d7e
IacobIonut01 <paulionut2003@gmail.com>
2023-10-26 18:50:01 +0300

    Gallery: Allow concurrency with mutex locking
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/src/main/kotlin/com/dot/gallery/core/MediaObserver.kt
+++ app/src/main/kotlin/com/dot/gallery/core/MediaObserver.kt
@@ -16,6 +16,8 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 /**
  * Register an observer class that gets callbacks when data identified by a given content URI
@@ -23,14 +25,17 @@ import kotlinx.coroutines.launch
  */
 fun Context.contentFlowObserver(uris: Array<Uri>) = callbackFlow {
     val ctx = this@contentFlowObserver
+    val mutex = Mutex()
     val observer = object : ContentObserver(null) {
         override fun onChange(selfChange: Boolean, uri: Uri?) {
             if (isActive) {
                 launch(Dispatchers.IO) {
-                    val mediaStoreVersion = MediaStore.getVersion(ctx)
-                    if (getStoredMediaVersion(ctx) != mediaStoreVersion) {
-                        updateStoredMediaVersion(ctx, mediaStoreVersion)
-                        send(selfChange)
+                    mutex.withLock {
+                        val mediaStoreVersion = MediaStore.getVersion(ctx)
+                        if (getStoredMediaVersion(ctx) != mediaStoreVersion) {
+                            updateStoredMediaVersion(ctx, mediaStoreVersion)
+                            trySend(selfChange)
+                        }
                     }
                 }
             }
@@ -39,8 +44,12 @@ fun Context.contentFlowObserver(uris: Array<Uri>) = callbackFlow {
     for (uri in uris)
         contentResolver.registerContentObserver(uri, true, observer)
     // trigger first.
-    updateStoredMediaVersion(ctx, MediaStore.getVersion(ctx))
-    trySend(false)
+    launch(Dispatchers.IO) {
+        mutex.withLock {
+            updateStoredMediaVersion(ctx, MediaStore.getVersion(ctx))
+            trySend(false)
+        }
+    }
     awaitClose {
         contentResolver.unregisterContentObserver(observer)
     }
