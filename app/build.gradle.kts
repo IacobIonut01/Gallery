7f0271d946fff196c672c05373dfa921d5c8c2e7
IacobIonut01 <paulionut2003@gmail.com>
2023-10-26 21:18:23 +0300

    Gradle: Set versionCode to 20070
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.dot.gallery"
         minSdk = 30
         targetSdk = 34
-        versionCode = 20051
+        versionCode = 20070
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
