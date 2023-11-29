627f264b29d22cc8d52be5f6f1aae5c18339a34d
IacobIonut01 <paulionut2003@gmail.com>
2023-11-29 19:33:54 +0200

    Blacklist: Improve design
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/src/main/kotlin/com/dot/gallery/feature_node/presentation/blacklist/BlacklistScreen.kt
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/blacklist/BlacklistScreen.kt
@@ -1,16 +1,20 @@
 package com.dot.gallery.feature_node.presentation.blacklist
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -22,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -69,43 +74,56 @@ fun BlacklistScreen(
             )
         }
     ) { paddingValues ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
-                .padding(top = 32.dp),
+                .padding(paddingValues),
         ) {
-            SettingsItem(item = SettingsEntity.Preference(
-                title = stringResource(R.string.add_to_blacklist),
-                summary = stringResource(R.string.add_to_blacklist_summary),
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+
+                if (state.albums.isNotEmpty()) {
+                    SettingsItem(
+                        item = SettingsEntity.Header(stringResource(R.string.blacklisted_albums))
+                    )
+                }
+
+                state.albums.forEachIndexed { index, blacklistedAlbum ->
+                    val position = remember(state.albums) {
+                        if (index == 0) {
+                            if (state.albums.size == 1) Position.Alone
+                            else Position.Top
+                        } else if (index == state.albums.size - 1) Position.Bottom
+                        else Position.Middle
+                    }
+                    SettingsItem(
+                        item = SettingsEntity.Preference(
+                            title = blacklistedAlbum.label,
+                            screenPosition = position,
+                            onClick = {
+                                toBeRemoved = blacklistedAlbum
+                            }
+                        )
+                    )
+                }
+            }
+
+            FloatingActionButton(
                 onClick = {
                     scope.launch {
                         selectAlbumState.show()
                     }
-                }
-            ))
-            if (state.albums.isNotEmpty()) {
-                SettingsItem(
-                    item = SettingsEntity.Header(stringResource(R.string.blacklisted_albums))
-                )
-            }
-
-            state.albums.forEachIndexed { index, blacklistedAlbum ->
-                val position = remember(state.albums) {
-                    if (index == 0) {
-                        if (state.albums.size == 1) Position.Alone
-                        else Position.Top
-                    } else if (index == state.albums.size - 1) Position.Bottom
-                    else Position.Middle
-                }
-                SettingsItem(
-                    item = SettingsEntity.Preference(
-                        title = blacklistedAlbum.label,
-                        screenPosition = position,
-                        onClick = {
-                            toBeRemoved = blacklistedAlbum
-                        }
-                    )
+                },
+                modifier = Modifier
+                    .padding(32.dp)
+                    .align(Alignment.BottomEnd),
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = null
                 )
             }
 
@@ -136,10 +154,12 @@ fun BlacklistScreen(
                 Text(text = stringResource(R.string.remove_from_blacklist))
             },
             text = {
-                Text(text = stringResource(
-                    R.string.remove_from_blacklist_summary,
-                    toBeRemoved!!.label
-                ))
+                Text(
+                    text = stringResource(
+                        R.string.remove_from_blacklist_summary,
+                        toBeRemoved!!.label
+                    )
+                )
             },
             properties = DialogProperties(
                 dismissOnBackPress = false,
