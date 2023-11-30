00616f021606c9f35a6fe5adb3424e19a35a4f2f
IacobIonut01 <paulionut2003@gmail.com>
2023-11-30 14:14:03 +0200

    Gradle: Update dependencies
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
     packaging {
         resources {
