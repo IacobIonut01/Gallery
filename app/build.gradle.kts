9a506c8898558c73af37ba187145a16beeca447b
IacobIonut01 <paulionut2003@gmail.com>
2024-02-11 12:06:56 +0200

    Compose: Enable strong skipping
    Improves overall performance
    
    ! - For lower end devices, especially tablets that renders lots of images at the same time, it might still hang, I'm actively looking for a solution about it
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += listOf("-P", "plugin:androidx.compose.compiler.plugins.kotlin:experimentalStrongSkipping=true")
     }
     buildFeatures {
         compose = true
