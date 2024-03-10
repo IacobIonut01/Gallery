396de276c8a613a70df47885529e8b2b4b75449f
IacobIonut01 <paulionut2003@gmail.com>
2024-03-10 14:38:56 +0200

    Gradle: Bump to 21040
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.dot.gallery"
         minSdk = 30
         targetSdk = 34
-        versionCode = 21036
+        versionCode = 21040
         versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
