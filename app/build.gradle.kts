IacobIonut01/Gallery

commit c8513ae702fb014723fd47514a012effca70f27d

Gallery: Strip all files permission for google play release (does not pass review)
Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.dot.gallery"
         minSdk = 30
         targetSdk = 34
-        versionCode = 21107
+        versionCode = 21108
         versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -69,6 +69,7 @@ android {
                 )
             )
             signingConfig = signingConfigs.getByName("release")
+            buildConfigField("Boolean", "ALLOW_ALL_FILES_ACCESS", allowAllFilesAccess)
             buildConfigField("String", "MAPS_TOKEN", getApiKey())
             buildConfigField("String", "CONTENT_AUTHORITY", "\"com.dot.gallery.media_provider\"")
         }
@@ -221,6 +222,19 @@ fun getApiKey(): String {
         "\"DEBUG\""
     }
 }
+
+val allowAllFilesAccess: String
+    get() {
+        val fl = rootProject.file("app.properties")
+
+        return try {
+            val properties = Properties()
+            properties.load(FileInputStream(fl))
+            properties.getProperty("ALL_FILES_ACCESS")
+        } catch (e: Exception) {
+            "true"
+        }
+    }
 
 @Suppress("UnstableApiUsage")
 val gitHeadVersion: String

.github/workflows/stable.yml
@@ -97,7 +97,9 @@ jobs:
         run: git config --global user.email "paulionut2003@gmail.com" &&
           git config --global user.name "IacobIonut01" &&
           git fetch https://github.com/IacobIonut01/Gallery.git main_play &&
-          git cherry-pick e63cd751baa1dc733222d5a3ce135cb7bc52868b
+          git cherry-pick e63cd751baa1dc733222d5a3ce135cb7bc52868b &&
+          chmod +x ./strip_allfiles_permission.sh && ./strip_allfiles_permission.sh &&
+          echo ALL_FILES_ACCESS=false > ./app.properties
       - name: Build Google Play Bundle
         run: ./gradlew bundleGplay
         env: 

app/.gitignore
@@ -1,2 +1,3 @@
 /build
 /release
+*.jks

app/src/main/AndroidManifest.xml
@@ -30,8 +30,7 @@
         (OPTIONAL)
         Requested to create albums outside Internal Storage/Pictures or Internal Storage/DCIM
     -->
-    <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE"
-        tools:ignore="ScopedStorage" />
+    <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE"/>
 
     <queries>
         <intent>

app/src/main/kotlin/com/dot/gallery/feature_node/presentation/setup/SetupScreen.kt
@@ -47,6 +47,7 @@ import com.dot.gallery.BuildConfig
 import com.dot.gallery.R
 import com.dot.gallery.core.Constants
 import com.dot.gallery.core.Settings.Misc.rememberIsMediaManager
+import com.dot.gallery.feature_node.presentation.util.isManageFilesAllowed
 import com.dot.gallery.feature_node.presentation.util.launchManageFiles
 import com.dot.gallery.feature_node.presentation.util.launchManageMedia
 import com.dot.gallery.ui.theme.GalleryTheme
@@ -189,13 +190,15 @@ fun SetupScreen(
                             withStyle(style = style) {
                                 append(stringResource(R.string.permission_manage_media_summary))
                             }
-                            appendLine()
-                            withStyle(style = style.copy(fontWeight = FontWeight.Bold)) {
-                                append(stringResource(R.string.permission_manage_files_title))
-                            }
-                            appendLine()
-                            withStyle(style = style) {
-                                append(stringResource(R.string.permission_manage_files_summary))
+                            if (isManageFilesAllowed) {
+                                appendLine()
+                                withStyle(style = style.copy(fontWeight = FontWeight.Bold)) {
+                                    append(stringResource(R.string.permission_manage_files_title))
+                                }
+                                appendLine()
+                                withStyle(style = style) {
+                                    append(stringResource(R.string.permission_manage_files_summary))
+                                }
                             }
                         }
                     }
@@ -219,7 +222,7 @@ fun SetupScreen(
                             Text(text = stringResource(R.string.allow_to_manage_media))
                         }
                     }
-                    if (!isStorageManager) {
+                    if (!isStorageManager && isManageFilesAllowed) {
                         Button(
                             onClick = {
                                 scope.launch {

app/src/main/kotlin/com/dot/gallery/feature_node/presentation/util/ContextExt.kt
@@ -2,6 +2,8 @@
  * Original from
  * https://stackoverflow.com/a/70853761/12978728
  */
+@file:Suppress("KotlinConstantConditions")
+
 package com.dot.gallery.feature_node.presentation.util
 
 import android.app.Activity
@@ -134,6 +136,9 @@ fun Context.launchManageMedia() {
     }
     startActivity(intent)
 }
+
+val isManageFilesAllowed: Boolean
+    get() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && BuildConfig.ALLOW_ALL_FILES_ACCESS
 
 @RequiresApi(Build.VERSION_CODES.S)
 fun Context.launchManageFiles() {

strip_allfiles_permission.sh
@@ -1,0 +1,2 @@
+sed -n '/<uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE"\/>/!p' app/src/main/AndroidManifest.xml > app/src/main/AndroidManifest2.xml
+mv app/src/main/AndroidManifest2.xml app/src/main/AndroidManifest.xml

commit 8cf98bce5b90962dc2fb3dab05b65567473b6718

Gallery: Add missing buildconfig for debug flavor
Fixes [BUG] Build failing in c8513ae702fb014723fd47514a012effca70f27d #378

Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-debug"
             manifestPlaceholders["appProvider"] = "com.dot.gallery.debug.media_provider"
+            buildConfigField("Boolean", "ALLOW_ALL_FILES_ACCESS", allowAllFilesAccess)
             buildConfigField(
                 "String",
                 "CONTENT_AUTHORITY",