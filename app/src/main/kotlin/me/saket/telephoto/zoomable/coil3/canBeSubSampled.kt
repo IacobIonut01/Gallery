668d72531f10a543144ca7b67db4cefb710257c1
IacobIonut01 <paulionut2003@gmail.com>
2024-03-30 13:19:36 +0200

    Gallery: Fix svg sub-sampling in Media View
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/src/main/kotlin/me/saket/telephoto/zoomable/coil3/canBeSubSampled.kt
+++ app/src/main/kotlin/me/saket/telephoto/zoomable/coil3/canBeSubSampled.kt
@@ -4,7 +4,8 @@ package me.saket.telephoto.zoomable.coil3
 
 import android.annotation.SuppressLint
 import android.util.TypedValue
-import coil.decode.DecodeUtils
+import coil3.decode.DecodeUtils
+import coil3.svg.isSvg
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import me.saket.telephoto.subsamplingimage.AssetImageSource
@@ -13,51 +14,47 @@ import me.saket.telephoto.subsamplingimage.RawImageSource
 import me.saket.telephoto.subsamplingimage.ResourceImageSource
 import me.saket.telephoto.subsamplingimage.SubSamplingImageSource
 import me.saket.telephoto.subsamplingimage.UriImageSource
-import me.saket.telephoto.zoomable.coil.isSvg
 import okio.FileSystem
 import okio.Source
 import okio.buffer
 import okio.source
+import okio.use
 
 context(Resolver)
 internal suspend fun SubSamplingImageSource.canBeSubSampled(): Boolean {
-  val preventSubSampling = when (this) {
-    is ResourceImageSource -> isVectorDrawable()
-    is AssetImageSource -> isSvgDecoderPresent() && isSvg()
-    is UriImageSource -> isSvgDecoderPresent() && isSvg()
-    is FileImageSource -> isSvgDecoderPresent() && isSvg(FileSystem.SYSTEM.source(path))
-    is RawImageSource -> isSvgDecoderPresent() && isSvg(source.invoke())
-  }
-  return !preventSubSampling
+    val preventSubSampling = when (this) {
+        is ResourceImageSource -> isVectorDrawable()
+        is AssetImageSource -> isSvgDecoderPresent() && isSvg()
+        is UriImageSource -> isSvgDecoderPresent() && isSvg()
+        is FileImageSource -> isSvgDecoderPresent() && isSvg(FileSystem.SYSTEM.source(path))
+        is RawImageSource -> isSvgDecoderPresent() && isSvg(source.invoke())
+    }
+    return !preventSubSampling
 }
 
 context(Resolver)
 private fun isSvgDecoderPresent(): Boolean {
-  // Searching for coil's SvgDecoder by name isn't the best idea,
-  // but it'll prevent opening of bitmap sources and inspecting
-  // them for SVGs for projects that don't need SVGs.
-  return imageLoader.components.decoderFactories.any {
-    it::class.qualifiedName?.contains("svg", ignoreCase = true) == true
-  }
+    // Only available in this app
+    return true
 }
 
 context(Resolver)
 private fun ResourceImageSource.isVectorDrawable(): Boolean =
-  TypedValue().apply {
-    request.context.resources.getValue(id, this, /* resolveRefs = */ true)
-  }.string.endsWith(".xml")
+    TypedValue().apply {
+        request.context.resources.getValue(id, this, /* resolveRefs = */ true)
+    }.string.endsWith(".xml")
 
 context(Resolver)
 private suspend fun AssetImageSource.isSvg(): Boolean =
-  isSvg(peek(request.context).source())
+    isSvg(peek(request.context).source())
 
 context(Resolver)
 @SuppressLint("Recycle")
 private suspend fun UriImageSource.isSvg(): Boolean =
-  isSvg(peek(request.context).source())
+    isSvg(peek(request.context).source())
 
 private suspend fun isSvg(source: Source?): Boolean {
-  return withContext(Dispatchers.IO) {
-    source?.buffer()?.use(DecodeUtils::isSvg) == true
-  }
+    return withContext(Dispatchers.IO) {
+        source?.buffer()?.use { DecodeUtils.isSvg(source.buffer()) } == true
+    }
 }
