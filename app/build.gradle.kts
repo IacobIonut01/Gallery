bef56ba0b2b8650e31094cc8b6cc599b0e2fb92f
IacobIonut01 <paulionut2003@gmail.com>
2024-11-03 21:40:47 +0200

    3.0.1-30131 Release
    Re-release of the 3.0.1 update
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.dot.gallery"
         minSdk = 30
         targetSdk = 35
-        versionCode = 30125
+        versionCode = 30131
         versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
