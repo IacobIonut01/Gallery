371d8fcb8773a4ac960b53337be10b24b725a87b
IacobIonut01 <paulionut2003@gmail.com>
2025-02-03 18:40:23 +0200

    Quick release of the Gallery 3.1.1 - 31120
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.dot.gallery"
         minSdk = 30
         targetSdk = 35
-        versionCode = 31118
+        versionCode = 31120
         versionName = "3.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
